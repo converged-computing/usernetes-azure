2bdbcfe029b33bae21d1bf48996751ae126528f3
Lise Jolicoeur <lisejoli@free.fr>
2025-02-04 16:10:58 +0100

    Update launch.sh

--- docker/resnet/launch.sh
+++ docker/resnet/launch.sh
@@ -1,12 +1,13 @@
-job_name="${1:-flux-sample}"
+job_name="${1:-flux-sample-}"
 nodes="${2:-2}"
 proc_per_node="${3:-4}"
 batch_size="${4:-32}"
+dns_suffix="${5:-.flux-service.default.svc.cluster.local}"
 
 export LOCAL_RANK=${FLUX_TASK_RANK}
 export RANK=${FLUX_TASK_RANK}
 export WORLD_SIZE=${nodes}
-MASTER_ADDR=${job_name}-0.flux-service.default.svc.cluster.local
+MASTER_ADDR=${job_name}0${dns_suffix}
 
 if [[ "${FLUX_TASK_RANK}" == "0" ]]; then
   echo "Torchrun for lead node"
