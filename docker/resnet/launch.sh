ba30c0a251a4b0dfc8be25da88f8002e42aabe37
Lise Jolicoeur <lisejoli@free.fr>
2025-02-05 12:44:14 +0100

    Update launch.sh

--- docker/resnet/launch.sh
+++ docker/resnet/launch.sh
@@ -2,11 +2,12 @@ job_name="${1:-flux-sample-}"
 nodes="${2:-2}"
 proc_per_node="${3:-4}"
 batch_size="${4:-32}"
-dns_suffix="${5:-.flux-service.default.svc.cluster.local}"
+dns_suffix="${5:-}"
 
 export LOCAL_RANK=${FLUX_TASK_RANK}
 export RANK=${FLUX_TASK_RANK}
 export WORLD_SIZE=${nodes}
+#.flux-service.default.svc.cluster.local
 MASTER_ADDR=${job_name}0${dns_suffix}
 
 if [[ "${FLUX_TASK_RANK}" == "0" ]]; then
