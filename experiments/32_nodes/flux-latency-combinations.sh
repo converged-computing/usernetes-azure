109e9f094fdbd9c063aaf07d6382f042bbc0aece
Lise Jolicoeur <lisejoli@free.fr>
2025-03-06 10:02:54 +0100

    Create flux-latency-combinations.sh

---
+++ experiments/32_nodes/flux-latency-combinations.sh
@@ -0,0 +1,32 @@
+mkdir -p ./results/osu_bw
+mkdir -p ./results/osu_latency
+
+# At most 28 combinations, 8 nodes 2 at a time
+hosts=$(flux run -N 32 hostname | shuf -n 8 | tr '\n' ' ')
+list=${hosts}
+
+dequeue_from_list() {
+  shift;
+  list=$@
+}
+
+iter=0
+for i in $hosts; do
+  dequeue_from_list $list
+  for j in $list; do
+    echo "${i} ${j}"
+    time flux submit run -N 2 -n 2 \
+      --setattr=user.study_id=osu_latency-2-iter-$iter \
+      --requires="hosts:${i},${j}" \
+      -o cpu-affinity=per-task \
+      singularity exec /opt/usernetes-azure_osu.sif \
+      /opt/osu-benchmark/build.openmpi/mpi/pt2pt/osu_latency
+    time flux submit -N 2 -n 2 \
+      --setattr=user.study_id=osu_bw-2-iter-$iter \
+      --requires="hosts:${i},${j}" \
+      -o cpu-affinity=per-task \
+      singularity exec /opt/usernetes-azure_osu.sif \
+      /opt/osu-benchmark/build.openmpi/mpi/pt2pt/osu_bw
+    iter=$((iter+1))
+done
+done
