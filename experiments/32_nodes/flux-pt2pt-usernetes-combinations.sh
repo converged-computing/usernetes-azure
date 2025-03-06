d057a7c80525b0543b01238b1c1e58a2d4b0d22d
Lise Jolicoeur <lisejoli@free.fr>
2025-03-06 10:10:00 +0100

    Create flux-pt2pt-usernetes-combinations.sh

---
+++ experiments/32_nodes/flux-pt2pt-usernetes-combinations.sh
@@ -0,0 +1,26 @@
+hosts=$(flux run -N 32 hostname | shuf -n 8 | tr '\n' ' ')
+list=${hosts}
+
+dequeue_from_list() {
+  shift;
+  list=$@
+}
+
+iter=0
+for i in $hosts; do
+  dequeue_from_list $list
+  for j in $list; do
+    echo "${i} ${j}"
+    flux submit run -N 2 -n 2 \
+      --setattr=user.study_id=osu_latency-2-iter-$iter \
+      --requires="hosts:${i},${j}" \
+      -o cpu-affinity=per-task \
+      /opt/osu-benchmark/build.openmpi/mpi/pt2pt/osu_latency
+    flux submit -N 2 -n 2 \
+      --setattr=user.study_id=osu_bw-2-iter-$iter \
+      --requires="hosts:${i},${j}" \
+      -o cpu-affinity=per-task \
+      /opt/osu-benchmark/build.openmpi/mpi/pt2pt/osu_bw
+    iter=$((iter+1))
+done
+done
